507fc53002522dc5c9346c44449578c321428ddc
yatendra <yatendrajain2014@gmail.com>
2016-10-07 17:00:23 +0530

    second

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  
+    
+$(".zeeTv").click(function() { 
+   $(".modal-body").find("iframe").remove();
+   $(".modal-title").text("Zee News");    
+   $(".modal-body").append("<iframe id='frmYT' width='100%' height='500' src='http://zeenews.dittotv.com/?header=no' frameborder='0' allowfullscreen='' scrolling='no'></iframe>")
+   
+ }); 
+    
+    
+$(".aajTak").click(function() { 
+   $(".modal-body").find("iframe").remove();
+    $(".modal-title").text("Aaj Tak");
+   $(".modal-body").append("<iframe name='test' src='http://feeds.intoday.in/video_stream/ltv/view_stream.php?siteId=2&amp;rand=0.7448911818496815' width='100%' height='500' id='PreviewVideo' scrolling='no' allowfullscreen=''></iframe>")
+   
+ });  
+    
+$(".demo").click(function() { 
+   $(".modal-body").find("iframe").remove();
+    $(".modal-title").text("Demo");
+   $(".modal-body").append("<iframe title='demo'></iframe>")
+   
+ });      
+    
+    
+    
+    $(".close").click(function() { 
+      $(this).parent(".modal-header").next("div").find("iframe").remove();
+    });
+    
+});
